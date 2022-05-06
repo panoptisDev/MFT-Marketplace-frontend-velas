ef0b599f9547ccbf330e7770b37114653f4b0368
xiaojang <fastmoon66@outlook.com>
2022-05-06 15:27:00 +0800

    remove js files

--- components/Profile/formatsortOptionLabel.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const formatsortOptionLabel = ({ value, label, customAbbreviation }) => (
-	<div style={{ display: "flex", alignItems: "center", background: "transparent", height: "40px" }}>
-		<div style={{color: "black", fontWeight: "bold", paddingLeft: "10px"}}>{label}</div>
-	</div>
-);
-
-export default formatsortOptionLabel;
