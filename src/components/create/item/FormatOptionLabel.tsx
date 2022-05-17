94fe83e2ee50215c046df88e0bc9e70162670c24
CryptoPiao <victorypiao@gmail.com>
2022-05-17 09:22:47 +0800

    Update FormatOptionLabel.tsx

--- src/components/create/item/FormatOptionLabel.tsx
+++ src/components/create/item/FormatOptionLabel.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
 type PropsType = {
-    value ? : string, 
-    label ? :string, 
-    customAbbreviation ? : any
+    name ? : string, 
+    logo_uri ? :string, 
+    description ? : string
 }
-export default function FormatOptionLabel({ value, label, customAbbreviation } : PropsType) {
+export default function FormatOptionLabel({ name, logo_uri, description } : PropsType) {
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
 		<div>
-			<img src={value === "ethereum" ? "/assets/img/parts/eth.png" : "/assets/img/parts/pol.svg"} alt="chain" width="40px"
+			<img src={logo_uri} alt="chain" width="40px"
 				 height="40px" style={{borderRadius: "50%"}} />
 		</div>
 		<div style={{display: "flex", flexDirection: "column", marginLeft: "10px"}}>
-			<div style={{color: "black", fontWeight: "bold"}}>{label}</div>
+			<div style={{color: "black", fontWeight: "bold"}}>{name}</div>
 			<div style={{ color: "black" }}>
-				{customAbbreviation}
+				{description}
 			</div>
 		</div>
 	</div>
